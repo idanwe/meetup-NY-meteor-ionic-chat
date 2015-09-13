7bd8ec31f8093741bfd6148405585bcf377bc5da
Idan Wender <idanwe2@gmail.com>
2015-09-14 01:18:13 +0300

    Add chat view

---
+++ README.md
@@ -0,0 +1,11 @@
+#### Step 0 - start
+
+1. Run `meteor create chat`
+2. Run `meteor add angular driftyco:ionic`
+3. Add `index.html`
+4. Add `app.js`
+
+#### Step 1 - chat view
+
+1. Run `meteor add fourseven:scss`
+2. Add chat template and CtrlController
--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 // Meteor Starter App
 if (Meteor.isClient) {
 
-  angular.module('chat', ['ionic'])
+  angular
+    .module('chat', ['ionic'])
+    .controller('ChatCtrl', ChatCtrl);
 
+  function ChatCtrl ($scope, $ionicScrollDelegate) {
+    var alternate;
+
+    $scope.data = {};
+    $scope.myId = '12345';
+    $scope.messages = [];
+
+    $scope.sendMessage = sendMessage;
+
+    ////////////
+
+    function sendMessage () {
+      if (!$scope.data.message || !$scope.data.message.length) return;
+
+      alternate = !alternate;
+      $scope.messages.push({
+        userId: alternate ? '12345' : '54321',
+        photo: alternate ?
+          'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/s720x720/11393068_920096478013144_1201901050919221507_n.jpg?oh=b67348e306c32edf97c6f0f82492d07d&oe=56729CF5' :
+          'https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/45011_147000805322719_4607640_n.jpg?oh=d6fb3a947911c210ee0ffe962111da22&oe=569E8FDF&__gda__=1453796800_84a78e328d1e7e17102a4079f94bf4ec',
+        text: $scope.data.message
+      });
+
+      delete $scope.data.message;
+      $ionicScrollDelegate.scrollBottom(true);
+    }
+  }
 }
