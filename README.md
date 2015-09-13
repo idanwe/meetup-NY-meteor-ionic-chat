424b0f398434f6d5f9dda3dea1a33cae4790146d
Idan Wender <idanwe2@gmail.com>
2015-09-14 01:43:59 +0300

    Add users

--- README.md
+++ README.md
@@ -15,3 +15,10 @@
 1. Add 'angular-meteor' as angular module
 2. Add `Messages = new Meteor.Collection('messages');` 
 3. Assign angular meteor collection to $scope `$scope.messages = $scope.$meteorCollection(Messages);`
+
+#### Step 3 - Users
+
+1. Run `meteor add accounts-password accounts-ui urigo:angular-blaze-template`
+2. Add to `index.html` - `<blaze-template name="loginButtons"></blaze-template>`
+3. Add to message wrapper div (`.message-list`) `ng-if="$root.currentUser`
+4. Add `currentUser._id` to message
--- app.js
+++ app.js
@@ -3,16 +3,18 @@
 Messages = new Meteor.Collection('messages');
 
 if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+  });
 
   angular
     .module('chat', ['ionic', 'angular-meteor'])
     .controller('ChatCtrl', ChatCtrl);
 
-  function ChatCtrl ($scope, $ionicScrollDelegate) {
+  function ChatCtrl ($rootScope, $scope, $ionicScrollDelegate) {
     var alternate;
 
     $scope.data = {};
-    $scope.myId = '12345';
     $scope.messages = $scope.$meteorCollection(Messages);
 
     $scope.sendMessage = sendMessage;
@@ -24,7 +26,7 @@ if (Meteor.isClient) {
 
       alternate = !alternate;
       $scope.messages.push({
-        userId: alternate ? '12345' : '54321',
+        userId: $rootScope.currentUser._id,
         photo: alternate ?
           'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/s720x720/11393068_920096478013144_1201901050919221507_n.jpg?oh=b67348e306c32edf97c6f0f82492d07d&oe=56729CF5' :
           'https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/45011_147000805322719_4607640_n.jpg?oh=d6fb3a947911c210ee0ffe962111da22&oe=569E8FDF&__gda__=1453796800_84a78e328d1e7e17102a4079f94bf4ec',
